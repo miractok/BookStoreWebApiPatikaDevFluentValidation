4455d98738088eee0a1abe6d476938999c23279a
Miraç Nurullah Tok <47780576+miractok@users.noreply.github.com>
2023-01-01 14:52:32 +0300

    Update UpdateBookValidator.cs

--- WebApi/BookOperations/UpdateBook/UpdateBookValidator.cs
+++ WebApi/BookOperations/UpdateBook/UpdateBookValidator.cs
@@ -7,7 +7,7 @@ namespace WebApi.BookOperations.UpdateBook
         public UpdateBookValidator()
         {
             RuleFor(command => command.BookID).GreaterThan(0);
-            RuleFor(command => command.Model.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.GenreId).GreaterThan(0).LessThan(4);
             RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(4);
         }
     }
