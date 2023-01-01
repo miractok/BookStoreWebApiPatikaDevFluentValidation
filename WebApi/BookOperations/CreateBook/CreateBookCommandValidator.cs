7830f7da03251741dbb83934c570d387770d59ad
Miraç Nurullah Tok <47780576+miractok@users.noreply.github.com>
2023-01-01 14:52:10 +0300

    Update CreateBookCommandValidator.cs

--- WebApi/BookOperations/CreateBook/CreateBookCommandValidator.cs
+++ WebApi/BookOperations/CreateBook/CreateBookCommandValidator.cs
@@ -6,7 +6,7 @@ namespace WebApi.BookOperations.CreateBook
     {
         public CreateBookCommandValidator()
         {
-            RuleFor(command => command.Model.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.GenreId).GreaterThan(0).LessThan(4);
             RuleFor(command => command.Model.PageCount).GreaterThan(0);
             RuleFor(command => command.Model.PublishDate.Date).NotEmpty().LessThan(DateTime.Now.Date);
             RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(4);
